78d3be057f527166daf45c14e8c203c5eb1cb732
Sam Streed <embersteelstreus@gmail.com>
2019-01-03 17:00:30 -0600

    Day 4.1: Started on bullets.

---
+++ Assets/Makhai/Core/Bullets/Bullet.cs
@@ -0,0 +1,79 @@
+﻿using Makhai.ComplexStats;
+using Makhai.Core.Data;
+using UnityEngine;
+
+namespace Makhai.Core.Bullets
+{
+	/// <summary>
+	/// A gameplay element that is used to perform some Entity damage interactions.
+	/// </summary>
+	[AddComponentMenu("Makhai/Bullet")]
+	public class Bullet : MonoBehaviour
+	{
+		#region INSTANCE_VARS
+
+		[SerializeField]
+		private BulletBehavior behavior;
+
+		[SerializeField]
+		private Timer lifetime;
+		#endregion
+
+		#region STATIC_METHODS
+
+		public static Bullet Create(BulletBehavior behavior, Entity source, Vector3 position, Quaternion rotation)
+		{
+			//TODO maybe a builder instead???
+		}
+		#endregion
+
+		#region INSTANCE_METHODS
+
+		public Bullet()
+		{
+			behavior = null;
+			lifetime = new Timer (float.PositiveInfinity);
+		}
+
+		#region EVENTS
+		private void Awake()
+		{
+			lifetime.Reset ();
+		}
+
+		private void Start()
+		{
+			behavior?.OnStart (this);
+		}
+
+		private void Update()
+		{
+			lifetime.Tick (Time.deltaTime);
+			behavior?.OnUpdate (this);
+		}
+
+		private void FixedUpdate()
+		{
+			behavior?.OnFixedUpdate (this);
+		}
+
+		private bool OnHit(Collider other)
+		{
+			return (behavior?.OnHit (this, other)).Value;
+		}
+
+		private void OnDeath()
+		{
+			behavior.OnDeath (this);
+			lifetime.Complete ();
+			Destroy (gameObject);
+		}
+
+		private void OnDestroy()
+		{
+			
+		}
+		#endregion
+		#endregion
+	}
+}
---
+++ Assets/Makhai/Core/Bullets/BulletBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Makhai.Core.Bullets
+{
+	[Serializable]
+	public abstract class BulletBehavior
+	{
+		public abstract GameObject GetPrefab();
+
+		public virtual void OnStart(Bullet subject) { }
+		public virtual void OnUpdate(Bullet subject) { }
+		public virtual void OnFixedUpdate(Bullet subject) { }
+		public virtual bool OnHit(Bullet subject, Collider other) { return true; }
+		public virtual void OnDeath(Bullet subject) { }
+	}
+}
